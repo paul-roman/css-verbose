d21478c5497c9357e85f27d593f1fd8c54e44a30
Paul Roman <paul.roman@epitech.eu>
2017-11-02 15:12:32 +0000

    Types finally fixed + dependencies are now dev

--- types.d.ts
+++ types.d.ts
@@ -1,46 +1,46 @@
-/* Units */
 declare module 'css-verbose' {
-	interface Number {
-		cm(): string;
-		mm(): string;
-		inch(): string;
-		px(): string;
-		pt(): string;
-		pc(): string;
-		em(): string;
-		ex(): string;
-		ch(): string;
-		rem(): string;
-		vw(): string;
-		vh(): string;
-		vMin(): string;
-		vMax(): string;
-		percents(): string;
+	/* Colors */
+	interface Color {
+		red: number;
+		green: number;
+		blue: number;
+		alpha: number;
+
+		new(hex: string): Color;
+		new(red: number, green: number, blue: number, alpha?: number): Color;
+
+		toHex(): string;
 	}
+	export function color(hex: string): Color;
+	export function color(red: number, green: number, blue: number, alpha?: number): Color;
+
+	export function rgb(red: number, green: number, blue: number): string;
+	export function rgba(red: number, green: number, blue: number, alpha: number): string;
+
+	/* Padding / Margin shorthand */
+	export function __(value1: string | number, value2?: string | number, value3?: string | number, value4?: string | number): string;
+	export function margin(value1: string | number, value2?: string | number, value3?: string | number, value4?: string | number): string;
+	export function padding(value1: string | number, value2?: string | number, value3?: string | number, value4?: string | number): string;
+
+	/* Border shorthand */
+	export function border(width: string | number, style?: string, color?: string): string;
 }
 
-/* Colors */
-declare interface Color {
-	red: number;
-	green: number;
-	blue: number;
-	alpha: number;
-
-	new(hex: string): Color;
-	new(red: number, green: number, blue: number, alpha?: number): Color;
-
-	toHex(): string;
+/* Units */
+interface Number {
+	cm(): string;
+	mm(): string;
+	inch(): string;
+	px(): string;
+	pt(): string;
+	pc(): string;
+	em(): string;
+	ex(): string;
+	ch(): string;
+	rem(): string;
+	vw(): string;
+	vh(): string;
+	vMin(): string;
+	vMax(): string;
+	percents(): string;
 }
-export function color(hex: string): Color;
-export function color(red: number, green: number, blue: number, alpha?: number): Color;
-
-export function rgb(red: number, green: number, blue: number): string;
-export function rgba(red: number, green: number, blue: number, alpha: number): string;
-
-/* Padding / Margin shorthand */
-export function __(value1: string | number, value2?: string | number, value3?: string | number, value4?: string | number): string;
-export function margin(value1: string | number, value2?: string | number, value3?: string | number, value4?: string | number): string;
-export function padding(value1: string | number, value2?: string | number, value3?: string | number, value4?: string | number): string;
-
-/* Border shorthand */
-export function border(width: string | number, style?: string, color?: string): string;
